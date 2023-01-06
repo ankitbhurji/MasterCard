fd824d6802a4b3220d2d7161bc051a63ea706b16
ankitbhurji <bhurji204@gmail.com>
2023-01-06 19:50:39 +0530

    added credit card component

---
+++ src/Card/Card.js
@@ -0,0 +1,59 @@
+import styles from './Card.module.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+function Card(props){
+
+    var Number, Name, Month, Cvc, Year;
+    let Num=props.CardData.Number 
+    let CvcNum = props.CardData.Cvc
+    let MonthNum = props.CardData.Month
+    let spacedNumber, NumLength, CvcNumLength, MonthLength
+
+    if(Num){
+        if(Num.length==16){
+            spacedNumber=Num.match(/.{1,4}/g).join(' ');
+            NumLength = Num.length;
+            CvcNumLength = CvcNum.length;
+            MonthLength = MonthNum.length;            
+        }else{
+            // alert("enter valid card number")
+        }
+    }
+
+
+    if((NumLength == 16) && (props.CardData.Name) && (MonthLength==2) && (CvcNumLength == 3)){
+        
+        Number = spacedNumber
+        Name = props.CardData.Name
+        Month = props.CardData.Month
+        Year = props.CardData.Year
+        Cvc = props.CardData.Cvc
+ 
+    }else{
+        Number = '0000 0000 0000 0000'
+        Name = 'ankit bhurji'
+        Month = "12"
+        Year = "24"
+        Cvc = '009'
+    }
+
+
+    return(
+        <div className={styles.card}>
+            <div className={styles.front}>
+                <p className={styles.cardNumber}>{Number}</p>
+                <p className={styles.cardName}>{Name}</p>
+                <p className={styles.cardDate}>{Month}/{Year}</p>
+            </div>
+            <div className={styles.back}>
+                <p className={styles.magnet}></p>
+                <p className={styles.cardCvc}>{Cvc}</p>
+            </div>
+            <ToastContainer />
+        </div>
+    )
+}
+
+export default Card
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,17 +1,25 @@
 import styles from './Home.module.css'
 import Form from '../Form/Form';
-
-
+import Card from '../Card/Card';
+import { useEffect, useState } from 'react';
 
 function Home(){
+
+    const [CardData, setCardData] = useState('');
 
     return(
         <div>
             <div>
+                <div className={styles.home_1}>
+                    <Card CardData={CardData}/>
+                </div>
+                
                 <div className={styles.home_2}>
-                    <Form />
+                    <Form setCardData={setCardData}/>
                 </div>
             </div>
+            
+            
         </div>
     );
 }
